9173c43dc3bf4f3a7ed707598c139116cfdb7d3b
Andi Hoerduin <andihoerudin24@gmail.com>
2021-09-12 10:29:33 +0700

    log

---
+++ application/logs/log-2021-09-12.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-09-12 05:28:54 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:54 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:54 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:54 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:55 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:55 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:55 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:56 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:56 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:56 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:56 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:57 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:57 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:57 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:57 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:58 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:58 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:58 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:59 --> 404 Page Not Found: /index
+ERROR - 2021-09-12 05:28:59 --> 404 Page Not Found: /index
